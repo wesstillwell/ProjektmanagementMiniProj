fd37cf29e160c6ee6d17514064c1ec0eab1f8ba6
wes <wesley.st96@yahoo.de>
2019-01-03 13:17:08 +0100

    changed folder structure

---
+++ ownProj/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ownProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This buid should fail");
+
+            int i = 1;
+
+            Console.WriteLine(i);
+
+            if (i == 1)
+            {
+                Console.WriteLine("YAY");
+            }
+        }
+    }
+}
