47969347724916544c2084f6eb26af4d51a04ecf
wes <wesley.st96@yahoo.de>
2019-01-03 13:20:35 +0100

    fixed

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ownProj
         {
             Console.WriteLine("This buid should fail");
 
-            int i = "a";
+            int i = 1;
 
             Console.WriteLine(i);
         }
